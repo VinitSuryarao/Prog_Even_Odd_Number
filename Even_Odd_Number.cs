df48582e6a717629d97eeeed99ffce4a211b30c3
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:03:34 +0530

    Add files via upload

---
+++ Even_Odd_Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Even Odd Number
+
+        static void Main(string[] args)
+        {
+            int n = 7;
+
+            if ( n%2==0)
+                Console.WriteLine("Even Number");
+            else
+                Console.WriteLine("Odd Number");
+
+        }
+    }
+
+
